6be3fcb6591acbd7657e80b31bbab29aa062f552
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2024-10-09 19:58:03 +0300

    navigating between the sidebar pages

--- src/app/utils/withAuth.jsx
+++ src/app/utils/withAuth.jsx
@@ -1,20 +1,36 @@
-import { useEffect } from 'react';
+// utils/withAuth.jsx
+"use client"; // Ensure this file runs on the client side
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Cookies from 'js-cookie';
+import { isAuthenticated } from './auth';
 
 const withAuth = (WrappedComponent) => {
-  return (props) => {
+  const AuthComponent = (props) => {
     const router = useRouter();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-      const token = Cookies.get('authToken');
-      if (!token) {
+      // Check if the user is authenticated
+      if (!isAuthenticated()) {
+        // If not authenticated, redirect to the login page
         router.push('/login');
+      } else {
+        setIsLoading(false); // Authentication is successful, show the component
       }
     }, [router]);
 
+    if (isLoading) {
+      // Optionally return a loading indicator or null while checking authentication
+      return <div>Loading...</div>;
+    }
+
     return <WrappedComponent {...props} />;
   };
+
+  // Set a display name for better debugging
+  AuthComponent.displayName = `WithAuth(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+  return AuthComponent;
 };
 
 export default withAuth;
